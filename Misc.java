60eaa50491b95aeeab061f41e0086d08b6162d50
Chirag Sehgal <chirag23176@iiitd.ac.in>
2024-12-09 04:38:06 +0530

    Update Misc.java

--- Misc.java
+++ Misc.java
@@ -1,1 +1,38 @@
-conparator
+// Iterating
+for (Map.Entry<String, Integer> entry : map.entrySet()) {
+    System.out.println(entry.getKey() + ": " + entry.getValue());
+}
+
+//Comparator
+import java.util.*;
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class AgeComparator implements Comparator<Student> {
+    public int compare(Student s1, Student s2) {
+        return Integer.compare(s1.age, s2.age);
+    }
+}
+
+public class ComparatorExample {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Alice", 22));
+        students.add(new Student("Bob", 20));
+
+        Collections.sort(students, new AgeComparator());
+        for (Student s : students) {
+            System.out.println(s.name + ": " + s.age);
+        }
+    }
+}
+
+//
